fa3dd5f50f5ffba490295e7e7480da28038f271b
Diana Alejandra Diaz Niño <139186201+Dianaalejandra1446@users.noreply.github.com>
2024-04-10 18:09:15 -0500

    Update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,195 @@
+-- Active: 1712777518478@@127.0.0.1@3306@organizacionbd
+drop DATABASE;
+drop table if exists cuota;
+drop table if exists datos_personales;
+drop table if exists envio;
+drop table if exists envio_alimentos;
+drop table if exists envio_ayuda_humanitaria;
+drop table if exists envio_ayuda_humanitaria_id_envio_alimentos;
+drop table if exists envio_ayuda_humanitaria_id_envio_medicamentos;
+drop table if exists envio_ayuda_humanitaria_id_voluntariados;
+drop table if exists envio_medicamentos;
+drop table if exists envios_sedes;
+drop table if exists roles;
+drop table if exists sedes;
+drop table if exists socios;
+drop table if exists usuario;
+drop table if exists usuario_roles;
+drop table if exists voluntarios;
+
+create table cuota (
+    valor int not null,
+    id_cuota bigint auto_increment,
+    tipo varchar(255),
+    primary key (id_cuota)
+);
+
+create table datos_personales (
+    documento double,
+    telefono int,
+    id_datos bigint auto_increment,
+    apellido varchar(255),
+    email varchar(255) not null unique,
+    nombre varchar(255),
+    primary key (id_datos)
+);
+
+create table envio (
+    codigo_envio double,
+    fecha_salida timestamp,
+    id_envio bigint auto_increment,
+    destino varchar(255),
+    nombre_refugio varchar(255),
+    primary key (id_envio)
+);
+
+create table envio_alimentos (
+    num_toneladas double,
+    id_envio_alimentos bigint auto_increment,
+    nombre_producto varchar(255),
+    primary key (id_envio_alimentos)
+);
+
+create table envio_ayuda_humanitaria (
+    num_voluntarios int,
+    id_ayuda_humanitaria bigint not null,
+    id_ayuda_material bigint auto_increment,
+    id_envio_sede bigint,
+    primary key (id_ayuda_material)
+);
+
+create table envio_ayuda_humanitaria_id_envio_alimentos (
+    envio_ayuda_material_entity_id_ayuda_material bigint not null,
+    id_envio_alimentos_id_envio_alimentos bigint not null,
+    unique(id_envio_alimentos_id_envio_alimentos)
+);
+
+create table envio_ayuda_humanitaria_id_envio_medicamentos (
+    envio_ayuda_material_entity_id_ayuda_material bigint not null,
+    id_envio_medicamentos_id_envio_medicamentos bigint not null,
+    unique(id_envio_medicamentos_id_envio_medicamentos)
+);
+
+create table envio_ayuda_humanitaria_id_voluntariados (
+    envio_ayuda_humanitaria_entity_id_ayuda_humanitaria bigint not null,
+    id_voluntariados_id_voluntariado bigint not null,
+    unique(id_voluntariados_id_voluntariado)
+);
+
+create table envio_medicamentos (
+    num_unidades int,
+    id_envio_medicamentos bigint auto_increment,
+    nombre_medicamento varchar(255),
+    primary key (id_envio_medicamentos)
+);
+
+create table envios_sedes (
+    id_envio bigint,
+    id_envio_sedes bigint auto_increment,
+    id_sede bigint,
+    primary key (id_envio_sedes)
+);
+
+create table roles (
+    id bigint auto_increment,
+    description varchar(255),
+    role_name varchar(255),
+    primary key (id)
+);
+
+create table sedes (
+    id bigint auto_increment,
+    id_socios_id bigint,
+    ciudad varchar(255),
+    domicilio varchar(255),
+    nombre_director varchar(255),
+    nombre_sede varchar(255),
+    primary key (id)
+);
+
+create table socios (
+    cuenta_bancaria double,
+    fecha_pago timestamp,
+    id bigint auto_increment,
+    id_cuota_id_cuota bigint,
+    id_datos_personales bigint,
+    primary key (id)
+);
+
+create table usuario (
+    cedula bigint not null unique,
+    fecha timestamp,
+    id bigint auto_increment,
+    email varchar(255),
+    pwd varchar(255),
+    primary key (id)
+);
+
+create table usuario_roles (
+    roles_id bigint not null,
+    usuario_id bigint not null,
+    unique(roles_id, usuario_id)
+);
+
+create table voluntarios (
+    numero_trabajos_participados int,
+    id_datos_personales bigint,
+    id_sede_id bigint,
+    id_voluntariado bigint auto_increment,
+    disponibilidad varchar(255) not null,
+    profesion varchar(255),
+    tipo varchar(255),
+    primary key (id_voluntariado)
+);
+
+alter table envio_ayuda_humanitaria 
+   add constraint FK9gm70th3t7sxdamosi9hh5doe 
+   foreign key (id_envio_sede) 
+   references envios_sedes(id_envio_sedes);
+
+alter table envio_ayuda_humanitaria_id_envio_alimentos 
+   add constraint FK3guk2cvgr0aos7vd5afw9bln3 
+   foreign key (id_envio_alimentos_id_envio_alimentos) 
+   references envio_alimentos(id_envio_alimentos);
+
+alter table envio_ayuda_humanitaria_id_envio_alimentos 
+   add constraint FKahrr2rjrth9qvyixrdn1l78o6 
+   foreign key (envio_ayuda_material_entity_id_ayuda_material) 
+   references envio_ayuda_humanitaria(id_ayuda_material);
+
+alter table envio_ayuda_humanitaria_id_envio_medicamentos 
+   add constraint FK3fkwv9ixc53umblkp7c78u7jv 
+   foreign key (id_envio_medicamentos_id_envio_medicamentos) 
+   references envio_medicamentos(id_envio_medicamentos);
+
+alter table envio_ayuda_humanitaria_id_envio_medicamentos 
+   add constraint FK18ine9y3ki4wx8od3cpoqbf5e 
+   foreign key (envio_ayuda_material_entity_id_ayuda_material) 
+   references envio_ayuda_humanitaria(id_ayuda_material);
+
+alter table envio_ayuda_humanitaria_id_voluntariados 
+   add constraint FKl1u4ob15rmls4rvftjjuc7wt4 
+   foreign key (id_voluntariados_id_voluntariado) 
+   references voluntarios(id_voluntariado);
+
+alter table envio_ayuda_humanitaria_id_voluntariados 
+   add constraint FKed0w22pivffjdo5yhnd7gdp7a 
+   foreign key (envio_ayuda_humanitaria_entity_id_ayuda_humanitaria) 
+   references envio_ayuda_humanitaria(id_ayuda_material);
+
+alter table envios_sedes 
+   add constraint FKda8dpplt199wn2e5j7i9tios8 
+   foreign key (id_envio) 
+   references envio(id_envio);
+
+alter table envios_sedes 
+   add constraint FKddclnbydgg0jctx
+
+
 -- INSERTAR USUARIOS
-INSERT INTO usuario (cedula, email, pwd, fecha) VALUES (1095299291, 'dianaleja@hotmail.com', 'to_be_encoded', '2015-12-01');
-INSERT INTO usuario (cedula, email, pwd, fecha) VALUES (1005372614, 'lauragomez@example.com', 'to_be_encoded_auxiliar', '2015-12-01');
-INSERT INTO usuario (cedula, email, pwd, fecha) VALUES (1005372615, 'carlosrodriguez@example.com', 'to_be_encoded_director', '2015-12-01');
+INSERT INTO usuario (cedula, email, pwd, fecha) VALUES (1095299292, 'dianaleja@hotmail.com', 'to_be_encoded', '2015-12-01');
+INSERT INTO usuario (cedula, email, pwd, fecha) VALUES (1005372684, 'kevin@example.com', 'to_be_encoded_auxiliar', '2015-12-01');
+INSERT INTO usuario (cedula, email, pwd, fecha) VALUES (100537265, 'rut@example.com', 'to_be_encoded_director', '2015-12-01');
 
 -- INSERTAR ROLES
 INSERT INTO roles (role_name, description) VALUES ('ROLE_ADMIN', 'puede ver el punto de cuenta'); 
@@ -51,99 +239,3 @@ INSERT INTO envio_ayuda_humanitaria (num_voluntarios, id_ayuda_humanitaria, id_e
 -- Inserts para la tabla 'voluntarios'
 INSERT INTO voluntarios (numero_trabajos_participados, id_datos_personales, id_sede_id, disponibilidad, profesion, tipo) VALUES (5, 1, 1, 'Sí', 'Médico', 'Sanitario');
 INSERT INTO voluntarios (numero_trabajos_participados, id_datos_personales, id_sede_id, disponibilidad, profesion, tipo) VALUES (3, 2, 2, 'Sí', 'ATS', 'Sanitario');
-/*-- SQLBook: Code
-CREATE TABLE `Datos_personales`(
-    `id_datos` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nombre` VARCHAR(255) NOT NULL,
-    `apellido` VARCHAR(255) NOT NULL,
-    `documento` DOUBLE NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `telefono` INT
-);
-CREATE TABLE `Envio_ayuda_humanitaria`(
-    `id_ayuda_humanitaria` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `num_voluntarios` INT,
-    `id_voluntario` INT NOT NULL,
-    `id_envio_sede` INT NOT NULL
-);
-CREATE TABLE `Cuota`(
-    `id_cuota` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `tipo` VARCHAR(255),
-    `valor` INT
-);
-CREATE TABLE `Socios`(
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `id_datos_personales` INT NOT NULL,
-    `cuenta_bancaria` DOUBLE,
-    `fecha_pago` DATE,
-    `id_cuota` INT NOT NULL
-);
-CREATE TABLE `Envio_medicamentos`(
-    `id_envio_medicamentos` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nombre_medicamento` VARCHAR(255) NOT NULL,
-    `num_unidades` INT NOT NULL
-);
-CREATE TABLE `Sedes`(
-    `id_sede` INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nombre_sede` VARCHAR(255) NOT NULL,
-    `ciudad` VARCHAR(255) NOT NULL,
-    `domicilio` VARCHAR(255) NOT NULL,
-    `nombre_director` VARCHAR(255) NOT NULL,
-    `id_socio` INT NOT NULL
-);
-CREATE TABLE `Envio`(
-    `id_envio` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `codigo_envio` INT NOT NULL,
-    `nombre_refugio` VARCHAR(255) NOT NULL,
-    `destino` VARCHAR(255),
-    `fecha_salida` DATE NOT NULL
-);
-CREATE TABLE `Envio_ayuda_material`(
-    `id_ayuda_material` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `id_envio_sede` INT NOT NULL,
-    `id_envio_alimentos` INT NOT NULL,
-    `id_envio_medicamento` INT NOT NULL
-);
-CREATE TABLE `Voluntariados`(
-    `id_voluntario` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `tipo` VARCHAR(255),
-    `profesion` VARCHAR(255),
-    `disponibilidad` VARCHAR(255),
-    `num_trabajos_participados` INT,
-    `id_datos_personales` INT NOT NULL,
-    `id_sede` INT NOT NULL
-);
-CREATE TABLE `Envios_Sedes`(
-    `id_envios_sede` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `id_envio` INT NOT NULL,
-    `id_sede` INT NOT NULL
-);
-CREATE TABLE `Envio_alimentos`(
-    `id_envio_alimentos` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nombre_producto` VARCHAR(255),
-    `num_toneladas` DOUBLE NOT NULL
-);
-ALTER TABLE
-    `Envio_ayuda_humanitaria` ADD CONSTRAINT `envio_ayuda_humanitaria_id_envio_sede_foreign` FOREIGN KEY(`id_envio_sede`) REFERENCES `Envios_Sedes`(`id_envios_sede`);
-ALTER TABLE
-    `Envio_ayuda_humanitaria` ADD CONSTRAINT `envio_ayuda_humanitaria_id_voluntario_foreign` FOREIGN KEY(`id_voluntario`) REFERENCES `Voluntariados`(`id_voluntario`);
-ALTER TABLE
-    `Voluntariados` ADD CONSTRAINT `voluntariados_id_sede_foreign` FOREIGN KEY(`id_sede`) REFERENCES `Sedes`(`id_sede`);
-ALTER TABLE
-    `Envios_Sedes` ADD CONSTRAINT `envios_sedes_id_sede_foreign` FOREIGN KEY(`id_sede`) REFERENCES `Sedes`(`id_sede`);
-ALTER TABLE
-    `Envio_ayuda_material` ADD CONSTRAINT `envio_ayuda_material_id_envio_alimentos_foreign` FOREIGN KEY(`id_envio_alimentos`) REFERENCES `Envio_alimentos`(`id_envio_alimentos`);
-ALTER TABLE
-    `Envios_Sedes` ADD CONSTRAINT `envios_sedes_id_envio_foreign` FOREIGN KEY(`id_envio`) REFERENCES `Envio`(`id_envio`);
-ALTER TABLE
-    `Voluntariados` ADD CONSTRAINT `voluntariados_id_datos_personales_foreign` FOREIGN KEY(`id_datos_personales`) REFERENCES `Datos_personales`(`id_datos`);
-ALTER TABLE
-    `Socios` ADD CONSTRAINT `socios_id_datos_personales_foreign` FOREIGN KEY(`id_datos_personales`) REFERENCES `Datos_personales`(`id_datos`);
-ALTER TABLE
-    `Envio_ayuda_material` ADD CONSTRAINT `envio_ayuda_material_id_envio_sede_foreign` FOREIGN KEY(`id_envio_sede`) REFERENCES `Envios_Sedes`(`id_envios_sede`);
-ALTER TABLE
-    `Envio_ayuda_material` ADD CONSTRAINT `envio_ayuda_material_id_envio_medicamento_foreign` FOREIGN KEY(`id_envio_medicamento`) REFERENCES `Envio_medicamentos`(`id_envio_medicamentos`);
-ALTER TABLE
-    `Socios` ADD CONSTRAINT `socios_id_cuota_foreign` FOREIGN KEY(`id_cuota`) REFERENCES `Cuota`(`id_cuota`);
-ALTER TABLE
-    `Sedes` ADD CONSTRAINT `sedes_id_socio_foreign` FOREIGN KEY(`id_socio`) REFERENCES `Socios`(`id`);*/
